a292d81be97e1ae4c3213bd5e73ba7984dd073ce
Oroko <aoroko09@gmail.com>
2020-09-04 10:44:40 -0400

    Created a Github style file list with react  and styled the components with css

--- src/index.js
+++ src/index.js
@@ -1,24 +1,87 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Time from './Time'
 import PropTypes from 'prop-types'
+import './index.css'
 
 const FileList = ({files}) => {
   return(
     <table className="file-list">
       <tbody>
         {files.map(file => (
-          <tr className="file-list-item" key={file.id}>
-            <td className="file-name">{file.name}</td>
-          </tr>
+          <FileListItem key ={file.id} file={file}/>
         ))}
       </tbody>
     </table>
   )
 }
-
 FileList.propTypes = {
   files:PropTypes.array
 }; 
+const FileListItem = ({file}) => {
+  return(
+    <tr className="file-list-item">
+      <td className="file-icon"> <FileIcon file = {file} /> </td>
+      <td className="file-name"><FileName file={file}/></td>
+      <td className="commit-message"><CommitMessage commit={file.latestCommit} /></td>
+      <td className="age">
+        <Time time ={file.updated_at} />
+      </td>
+      
+    </tr>
+  )
+  
+}
+
+FileListItem.propTypes={
+  file:PropTypes.object.isRequired
+}
+
+const FileName = ({file}) => {
+  return(
+    <>
+    
+      {file.name}
+     
+    </> 
+   
+  )
+   
+}
+FileName.propTypes={
+  name:PropTypes.object.isRequired
+}
+
+const FileIcon = ({file}) => {
+  let icon = 'fa-file-text-o';
+  if(file.type === "folder"){
+    icon = 'fa-folder';
+  }
+  return(
+   <>
+      <i className={`fa ${icon}`} />
+    </>
+  )
+}
+
+FileIcon.propTypes ={
+  file:PropTypes.object.isRequired
+}
+
+const CommitMessage = ({commit}) => {
+  return(
+    <>
+      {commit.message}
+      </>
+    
+  )
+}
+CommitMessage.propTypes={
+  commit:PropTypes.object.isRequired
+}
+
+
+
 
 const testFiles = [
   {
@@ -45,7 +108,7 @@ const testFiles = [
   type : 'file',
   updated_at : "2016-07-18 21:24:00",
   latestCommit : {
-  message : 'Added a readme'
+  message : 'Added a readme '
   }
   },
   ];
